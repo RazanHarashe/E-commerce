4b635a65b1e846e2a640951d9c000170d6f6c192
RazanHarashe <harashe115@gmail.com>
2023-10-21 23:32:27 +0300

    first commit

---
+++ src/modules/categories/categories.router.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as categoriesController from './categories.controller.js';
+const router =Router();
+
+router.get('/',categoriesController.getCategories)
+   
+
+ 
+export default router;
