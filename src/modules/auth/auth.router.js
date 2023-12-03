805e5362dee937b963b25d987819688d352db561
RazanHarashe <harashe115@gmail.com>
2023-12-03 02:41:23 +0200

    update in confirm email & add confirm email router

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -5,7 +5,8 @@ import { asyncHandler } from "../../services/errorHandling.js";
 const router = Router();
 router.post('/signup',fileUpload(fileValidation.image).single('image'),asyncHandler(AuthController.signUp));
 router.post('/signin',asyncHandler(AuthController.signIn));
-router.patch('/sendCode',asyncHandler(AuthController.sendCode))
+router.get('/confirmEmail/:token', asyncHandler(AuthController.confirmEmail));
+router.patch('/sendCode',asyncHandler(AuthController.sendCode));
 router.patch('/forgotPassword',asyncHandler(AuthController.forgotPassword))
 router.delete('/invalidConfirm',asyncHandler(AuthController.deleteInvalidConfirm))
 export default router;
